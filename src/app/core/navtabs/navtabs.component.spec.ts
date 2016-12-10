e9f91fa748d725086dc887a74ea155127fe34b8b
Yash Kapila <yash.kapila18@gmail.com>
2016-12-10 12:34:12 +1100

    Add files via upload

---
+++ src/app/core/navtabs/navtabs.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { NavtabsComponent } from './navtabs.component';
+
+describe('NavtabsComponent', () => {
+  let component: NavtabsComponent;
+  let fixture: ComponentFixture<NavtabsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavtabsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavtabsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
