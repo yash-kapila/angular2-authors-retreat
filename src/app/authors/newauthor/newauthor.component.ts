e644f29dc4098da4cfb47cc455dab2672f65f852
Yash Kapila <yash.kapila18@gmail.com>
2016-12-10 12:11:38 +1100

    Create newauthor.component.ts

---
+++ src/app/authors/newauthor/newauthor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newauthor',
+  templateUrl: './newauthor.component.html',
+  styleUrls: ['./newauthor.component.css']
+})
+export class NewauthorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
