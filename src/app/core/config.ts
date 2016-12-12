a81b0263799fb86cb5f7c07f97d04ff779234864
Yash Kapila <yash.kapila18@gmail.com>
2016-12-12 18:39:36 +1100

    Update config.ts

--- src/app/core/config.ts
+++ src/app/core/config.ts
@@ -1,12 +1,18 @@
 export let CONFIG = {
   baseUrls: {
     authors: {
-        allAuthors: 'api/authors',
+        allAuthors: 'api/author',
         newAuthor: 'api/author/new',
         deleteAuthor: 'api/author/delete',
         updateAuthor: 'api/author/update',
-        authorById: '/api/author'
+        authorById: 'api/author'
     },
-    books: 'api/books'
+    books: {
+        allBooks: 'api/book',
+        newBook: 'api/book/new',
+        deleteBook: 'api/book/delete',
+        updateBook: 'api/book/update',
+        bookById: 'api/book'
+    }
   }
 };
