680fb34d47c35088b7828eb33921ae6df008d3a8
Yash Kapila <yash.kapila18@gmail.com>
2016-12-12 13:02:41 +1100

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -30,7 +30,7 @@ router.get('/author/:id', function(req, res){
             res.status(200).json(record);
         }
     })
-})
+});
 
 router.post('/author/new', function(req, res){
     var author = new Authors({
@@ -40,13 +40,30 @@ router.post('/author/new', function(req, res){
     });
 
     author.createNewAuthor(function(err, record){
-        console.log('inside route createNewAuthor');
         if(err) res.status(500).json("Error while creating new author");
         else{
             res.status(200).json(record);
         }
     })
-})
+});
+
+router.put('/author/update', function(req, res){
+    var author = new Authors({
+        id: req.body.id,
+        name: req.body.name,
+        country: req.body.country
+    });
+
+    author.updateAuthorDetails(function(err, record){
+        if(err) res.status(500).json("Error while updating author details");
+        else{
+           res.status(200).json({
+               success: true,
+               message: "Author details were updated"
+           });
+        }
+    })
+});
 
 router.delete('/author/delete/:id', function(req, res){
     var author = new Authors({
@@ -62,7 +79,7 @@ router.delete('/author/delete/:id', function(req, res){
            });
        }
     })
-})
+});
 
 // expose router           
 exports = module.exports = router;
