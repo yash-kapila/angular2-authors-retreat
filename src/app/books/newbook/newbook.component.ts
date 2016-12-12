cec2a89abf7e7a6009cd9419a202e32a9ef60d57
Yash Kapila <yash.kapila18@gmail.com>
2016-12-12 15:53:44 +1100

    Update newbook.component.ts

--- src/app/books/newbook/newbook.component.ts
+++ src/app/books/newbook/newbook.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BooksService } from '../../core/services/books.service';
+import { BooksModel } from '../../models/books.model';
 
 @Component({
   selector: 'app-newbook',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newbook.component.css']
 })
 export class NewbookComponent implements OnInit {
+  book: BooksModel;
 
-  constructor() { }
+  constructor(
+    private _booksService: BooksService,
+    private _router: Router
+  ) { 
+    this.book = new BooksModel();
+  }
 
   ngOnInit() {
+  }
+
+  cancel(): void{
+    this._router.navigate(['/books']);
+  }
+
+  save(): void{
+    this.book.id = new Date().getTime();
+
+    this._booksService.saveNewBook(this.book).subscribe(
+      data => {
+        this._router.navigate(['/books']);
+      }
+    )
   }
 
 }
