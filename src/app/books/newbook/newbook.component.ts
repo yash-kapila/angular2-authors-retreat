c01184cac9d3f32f1a4fbbb1bd407803102d3715
Yash Kapila <yash.kapila18@gmail.com>
2016-12-12 14:26:25 +1100

    Add files via upload

---
+++ src/app/books/newbook/newbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newbook',
+  templateUrl: './newbook.component.html',
+  styleUrls: ['./newbook.component.css']
+})
+export class NewbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
