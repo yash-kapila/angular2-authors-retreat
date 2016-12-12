f691654a61a9b60a7785272f1a4f80f5197b6bc3
Yash Kapila <yash.kapila18@gmail.com>
2016-12-12 13:06:09 +1100

    Create filter-text.pipe.spec.ts

---
+++ src/app/shared/filter-text/filter-text.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { FilterAuthorsPipe } from './filter-authors.pipe';
+
+describe('FilterAuthorsPipe', () => {
+  it('create an instance', () => {
+    let pipe = new FilterAuthorsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
