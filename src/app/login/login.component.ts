f8ae584187f7a9d76080954f19913c978290594e
Yash Kapila <yash.kapila18@gmail.com>
2016-12-13 12:15:57 +1100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserprofileService } from '../core/services/userprofile.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isUserLoggedIn: boolean;
 
-  constructor() { }
+  constructor(
+    private _userProfileService: UserprofileService
+  ) { }
 
   ngOnInit() {
+    this.isUserLoggedIn = this._userProfileService.getLoggedInStatus();
+  }
+
+  login(): void{
+    this.isUserLoggedIn = true;
+    this._userProfileService.setLoggedInStatus(this.isUserLoggedIn);
+  }
+
+  logout(): void{
+    this.isUserLoggedIn = false;
+    this._userProfileService.setLoggedInStatus(this.isUserLoggedIn);
   }
 
 }
