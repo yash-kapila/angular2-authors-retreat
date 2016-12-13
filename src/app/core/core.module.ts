63839d6212d5668515af0e937483d3f023408e82
Yash Kapila <yash.kapila18@gmail.com>
2016-12-13 13:28:59 +1100

    Update core.module.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,35 +1,41 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { NavtabsComponent } from './navtabs/navtabs.component';
 import { ModalComponent } from './modal/modal.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 import { AuthorsService } from './services/authors.service';
 import { BooksService } from './services/books.service';
 import { UserprofileService } from './services/userprofile.service';
 import { AuthguardService } from './services/authguard.service';
+import { SpinnerService } from './spinner/spinner.service';
 
 @NgModule({
   imports: [
-    CommonModule,
     RouterModule,
-    HttpModule
+    HttpModule,
+    SharedModule
   ],
   declarations: [
     NavtabsComponent,
-    ModalComponent
+    ModalComponent,
+    SpinnerComponent
   ],
   providers:[
     AuthorsService,
     BooksService,
     UserprofileService,
-    AuthguardService
+    AuthguardService,
+    SpinnerService
   ],
   exports: [   
     NavtabsComponent,
-    ModalComponent
+    ModalComponent,
+    SpinnerComponent
   ]
 })
 export class CoreModule { } 
