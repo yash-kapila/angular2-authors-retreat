3057686bfb5bcd8fa8a9c1c0a4cb0bfd925724d1
Yash Kapila <yash.kapila18@gmail.com>
2016-12-13 12:13:51 +1100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthguardService } from './core/services/authguard.service';
+
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule' },
   { path: 'authors', loadChildren: 'app/authors/authors.module#AuthorsModule' },
   { path: 'books', loadChildren: 'app/books/books.module#BooksModule' },
-  { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' },
+  { 
+    path: 'admin', 
+    loadChildren: 'app/admin/admin.module#AdminModule',
+    canActivate: [
+      AuthguardService
+    ] 
+  },
   { path: 'login', loadChildren: 'app/login/login.module#LoginModule' }
 ];
 
