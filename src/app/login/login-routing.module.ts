332c3e78470b5b7ff12f2f5078e8190ba2fccf47
Yash Kapila <yash.kapila18@gmail.com>
2016-12-13 12:15:34 +1100

    Add files via upload

---
+++ src/app/login/login-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ]
+})
+export class LoginRoutingModule { }
